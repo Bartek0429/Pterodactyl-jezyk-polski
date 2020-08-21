b5396b4803bd49701524d6ba6b0ce79fcd89cef5
Bartek0429 <Bartek0429@users.noreply.github.com>
2020-08-21 13:35:32 +0200

    Pterodactyl jezyk polski
    Pterodactyl jezyk polski

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # Pterodactyl jezyk polski
  Tlumaczenie panelu pterodactyl na jezyyk polski.
+
+Folder "pl" wrzuć do /var/www/pterodactyl/resources/lang
+
+Zalecam uzyć poprzez putty
+
+php artisan migrate
+php artisan view:clear
+php artisan cache:clear
+
+w /var/www/pterodactyl
+
+Tłumaczenie nie jest skończone w 100% jeśli masz jakiś uwagi napisz :)
+
+Discord: ! SSman#8280
---
+++ pl/navigation.php
@@ -0,0 +1,32 @@
+<?php
+
+return [
+    'home' => 'Home',
+    'account' => [
+        'header' => 'ZARZĄDZANIE KONTEM',
+        'my_account' => 'Moje konto',
+        'security_controls' => 'Bezpieczenstwo',
+        'api_access' => 'Account API',
+        'my_servers' => 'Moje serwery',
+    ],
+    'server' => [
+        'header' => 'ZARZĄDZANIE SERWEREM',
+        'console' => 'Konsola',
+        'console-pop' => 'Fullscreen Console',
+        'file_management' => 'Web FTP',
+        'file_browser' => 'File Browser',
+        'create_file' => 'Create File',
+        'upload_files' => 'Upload Files',
+        'subusers' => 'Subusers',
+        'schedules' => 'Harmonogramy',
+        'configuration' => 'Konfiguracja',
+        'port_allocations' => 'Ustawienia alokacji',
+        'sftp_settings' => 'Ustawienia SFTP',
+        'startup_parameters' => 'Parametr startowy',
+        'databases' => 'Bazy danych',
+        'edit_file' => 'Edit File',
+        'admin_header' => 'Adminowe',
+        'admin' => 'Konfiguracja serwera',
+        'server_name' => 'Nazwa serwera',
+    ],
+];
